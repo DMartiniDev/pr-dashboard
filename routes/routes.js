e4993813ba1cbc285bcebe87135edcded27b2853
Dave Martinez <dkm.coder@gmail.com>
2018-05-07 15:33:24 +0200

    Feat: Add websockets to the server and create a new route used for testing

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,7 @@
 const authGithubController = require('../controllers/auth.github.controller');
+const webSocketController = require('../controllers/websockets.controller')
 
 module.exports = app => {
   app.get('/api/v1/auth/github', authGithubController.hello);
+  app.get('/pr-update', webSocketController.test);
 };
