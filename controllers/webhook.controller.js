015c018eddc39ffd6a2c788eda5a5976930bc1ea
g0g11 <goran.plavsic@gmail.com>
2018-05-07 14:52:24 +0200

    feat: fixed payload handling

--- controllers/webhook.controller.js
+++ controllers/webhook.controller.js
@@ -2,21 +2,21 @@ const mongoose = require('mongoose');
 const Pullrequest = mongoose.model('pullrequests');
 
 module.exports.newEvent = async (req, res) => {
-  console.log(req.body);
+  console.log(req.body.pull_request);
   try {
     const pullrequest = new Pullrequest({
-      githubId: req.body.payload.pull_request.id,
-      action: req.body.payload.action,
-      number: req.body.payload.number,
-      webUrl: req.body.payload.pull_request.html_url,
-      apiUrl: req.body.payload.pull_request.url,
-      state: req.body.payload.pull_request.state,
-      title: req.body.payload.pull_request.title,
-      comment: req.body.payload.pull_request.body,
-      created_at: req.body.payload.pull_request.created_at,
-      updated_at: req.body.payload.pull_request.updated_at,
-      closed_at: req.body.payload.pull_request.closed_at,
-      merged_at: req.body.payload.pull_request.merged_at,
+      githubId: req.body.pull_request.id,
+      action: req.body.action,
+      number: req.body.number,
+      webUrl: req.body.pull_request.html_url,
+      apiUrl: req.body.pull_request.url,
+      state: req.body.pull_request.state,
+      title: req.body.pull_request.title,
+      comment: req.body.pull_request.body,
+      created_at: req.body.pull_request.created_at,
+      updated_at: req.body.pull_request.updated_at,
+      closed_at: req.body.pull_request.closed_at,
+      merged_at: req.body.pull_request.merged_at,
     });
 
     await pullrequest.save();
