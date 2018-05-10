73c483848e55be58c7f02c99fab125c05ca3190a
g0g11 <goran.plavsic@gmail.com>
2018-05-10 17:17:14 +0200

    feat: handled promise rejection with node

--- bin/cronjob.js
+++ bin/cronjob.js
@@ -19,75 +19,76 @@ const USER_EMAIL = '/user/emails';
 
 async function cronjob() {
   // Fetch all public repositories and store in DB
-  try {
-    const allUsers = await User.find();
-    await allUsers.forEach(async user => {
-      const axiosConfig = {
-        headers: { Authorization: 'token ' + user.accessToken },
+  const allUsers = await User.find();
+  await allUsers.forEach(async user => {
+    const axiosConfig = {
+      headers: { Authorization: 'token ' + user.accessToken },
+    };
+
+    const fetchRepos = await axios.get(
+      `${BASE_URL}${PUBLIC_REPO}`,
+      axiosConfig,
+    );
+    const fetchEmail = await axios.get(
+      `${BASE_URL}${USER_EMAIL}`,
+      axiosConfig,
+    );
+
+    // Update user data
+    await user.update({
+      email: fetchEmail.data[0].email,
+    });
+
+    fetchRepos.data.forEach(async publicRepos => {
+      const existingRepo = await Repository.findOne({
+        githubId: publicRepos.id,
+      });
+
+      const values = {
+        githubId: publicRepos.id,
+        name: publicRepos.name,
+        fullName: publicRepos.full_name,
+        private: publicRepos.private,
+        webUrl: publicRepos.html_url,
+        apiUrl: publicRepos.url,
+        hookUrl: publicRepos.hooks_url,
+        pullUrl: publicRepos.pulls_url,
+        description: publicRepos.description,
+        language: publicRepos.language,
+        created_at: publicRepos.created_at,
+        updated_at: publicRepos.updated_at,
       };
 
-      const fetchRepos = await axios.get(
-        `${BASE_URL}${PUBLIC_REPO}`,
-        axiosConfig,
-      );
-      const fetchEmail = await axios.get(
-        `${BASE_URL}${USER_EMAIL}`,
-        axiosConfig,
-      );
-
-      // Update user data
-      await user.update({
-        email: fetchEmail.data[0].email,
-      });
-
-      fetchRepos.data.forEach(async publicRepos => {
-        const existingRepo = await Repository.findOne({
-          githubId: publicRepos.id,
+      if (!existingRepo) {
+        await new Repository(values).save();
+        await user.update({
+          $push: { _repositories: { repository: publicRepos._id } },
         });
 
-        const values = {
-          githubId: publicRepos.id,
-          name: publicRepos.name,
-          fullName: publicRepos.full_name,
-          private: publicRepos.private,
-          webUrl: publicRepos.html_url,
-          apiUrl: publicRepos.url,
-          hookUrl: publicRepos.hooks_url,
-          pullUrl: publicRepos.pulls_url,
-          description: publicRepos.description,
-          language: publicRepos.language,
-          created_at: publicRepos.created_at,
-          updated_at: publicRepos.updated_at,
+        // Create new Webhook
+        const webhookData = {
+          name: 'web',
+          active: true,
+          events: ['pull_request'],
+          config: {
+            url: keys.githubWebhookUrl,
+            content_type: 'json',
+            secret: keys.githubWebhookSecret,
+          },
         };
 
-        if (!existingRepo) {
-          await new Repository(values).save();
-          await user.update({
-            $push: { _repositories: { repository: publicRepos._id } },
-          });
-
-          // Create new Webhook
-          const webhookData = {
-            name: 'web',
-            active: true,
-            events: ['pull_request'],
-            config: {
-              url: keys.githubWebhookUrl,
-              content_type: 'json',
-              secret: keys.githubWebhookSecret,
-            },
-          };
-
-          await axios.post(publicRepos.hooks_url, webhookData, axiosConfig);
-        } else {
-          await existingRepo.update(values);
-        }
-      });
+        await axios.post(publicRepos.hooks_url, webhookData, axiosConfig);
+      } else {
+        await existingRepo.update(values);
+      }
     });
-    console.log('Cronjob successful finished!');
-  } catch (e) {
-    console.log(e);
-  }
+  });
+  console.log('Cronjob successful finished!');
 }
+
+// Handle Promise rejection
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at:', p, 'reason:', reason);
+});
 
 cronjob();
