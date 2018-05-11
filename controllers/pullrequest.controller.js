1b644a02cc6c6d4fc7a0c2639ca3da1618cd46fb
g0g11 <goran.plavsic@gmail.com>
2018-05-11 16:56:38 +0200

    feat: pull request controller updated to send specific data

--- controllers/pullrequest.controller.js
+++ controllers/pullrequest.controller.js
@@ -7,15 +7,31 @@ require('../services/raven');
 
 module.exports.listAll = async (req, res) => {
   try {
-    const pullrequests = await Pullrequest.find({}).populate('repository')
-    // const pullrequests = await User
-    //   .find({ _id: req.user.id })
-    //   .populate({
-    //     path: '_repositories.repository',
-    //     populate: {
-    //       path: '_pullRequests.pullRequest',
-    //     },
-    //   });
+    const pullrequests = await Pullrequest.find(
+      { owner: req.user.id },
+      {
+        user: true,
+        closed_at: true,
+        merged_at: true,
+        created_at: true,
+        updated_at: true,
+        action: true,
+        number: true,
+        webUrl: true,
+        state: true,
+        title: true,
+        review: true,
+        comment: true,
+        comments: true,
+        repository: true,
+      },
+    ).populate('repository', {
+      name: true,
+      fullName: true,
+      private: true,
+      webUrl: true,
+      description: true,
+    });
     res.status(200).send(pullrequests);
   } catch (e) {
     Raven.captureException(e);
