b463c4a31b05acb73cf2fc959d60e17b4ac87412
g0g11 <goran.plavsic@gmail.com>
2018-05-16 22:59:47 +0200

    chore: added wildcard

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ socket.removeSockets();
 // Middlewares
 app.use(
   cors({
-    origin: [keys.clientUrl, 'https://pure-client.herokuapp.com'],
+    origin: '*',
   }),
 );
 app.use(bodyParser.json());
