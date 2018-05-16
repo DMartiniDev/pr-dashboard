b05f6a66926b866020e34d1a8b0c49cc50121f4d
g0g11 <goran.plavsic@gmail.com>
2018-05-17 00:16:49 +0200

    feat: axios config

--- controllers/webhook.controller.js
+++ controllers/webhook.controller.js
@@ -125,15 +125,18 @@ module.exports.enable = async (req, res) => {
       secret: keys.githubWebhookSecret,
     },
   };
-  // const axiosConfig = {
-  //   headers: { Authorization: 'token ' + req.user.accessToken },
-  // };
+  const axiosConfig = {
+    headers: {
+      Authorization: 'token ' + req.user.accessToken,
+      'Access-Control-Allow-Origin': '*',
+    },
+  };
   try {
-    // const webhook = await axios.post(repo.hookUrl, webHookData, axiosConfig);
-    // await repo.update({
-    //   hookEnabled: true,
-    //   hookId: webhook.data.id,
-    // });
+    const webhook = await axios.post(repo.hookUrl, webHookData, axiosConfig);
+    await repo.update({
+      hookEnabled: true,
+      hookId: webhook.data.id,
+    });
     await pullrequestController.update(repo, req.user);
 
     res.status(200).send({ id: repo._id });
